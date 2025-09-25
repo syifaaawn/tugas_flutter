c5fea58b4bd166760d2ad9a320f38603f5e18c40
syifaaawn <syifawahyuningsih25@gmail.com>
2025-09-25 21:12:29 +0700

    membuat container

--- lib/tugas/quis.dart
+++ lib/tugas/quis.dart
@@ -7,15 +7,36 @@ class Quis extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(20),
+        padding: EdgeInsets.all(50),
         child: Column(
            crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text("Training",style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold )),
-              Icon(Icons.calendar_today),
+              Icon(Icons.calendar_month),
             ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Your Program", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              Text("Details>", style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.blue),),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                padding: EdgeInsets.all(20),
+                height: 200,
+                width:625,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  gradient: LinearGradient(colors: [const Color.fromARGB(255, 223, 61, 251), const Color.fromARGB(255, 239, 227, 241)])
+                ),
+              )],
           )
         ],
           
